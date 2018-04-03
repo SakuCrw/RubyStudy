30a5449fa54f3488b5a23ca75e5892fcd2054cb4
SakuCrw <crwhsacc@gmail.com>
2018-04-03 18:50:15 +0900

    20180403

--- ri20min.rb
+++ ri20min.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 
 class MegaGreeter
-    attr_accessor : names
+    attr_accessor :names
 
     #Create the object
     def initialize(names = "World")
@@ -22,6 +22,41 @@ class MegaGreeter
         end
     end
 
-aaaaaaa
+    # say bye to everbody
+    def say_by 
+        if @names.nil? 
+            puts "..."
+        elsif @names.respond_to?("join")
+            #join the list elements with commas
+            puts "Goodby #{@names.join(", ")}, come back soon!"
+        else
+            puts "Goodby #{@names}, come back soon!"
+        end
+
+    end
+
 end
 
+
+if __FILE__ == $0
+    mg = MegaGreeter.new
+    mg.say_hi
+    mg.say_bye
+  
+    # Change name to be "Zeke"
+    mg.names = "Zeke"
+    mg.say_hi
+    mg.say_bye
+  
+    # Change the name to an array of names
+    mg.names = ["Albert", "Brenda", "Charles",
+                "Dave", "Engelbert"]
+    mg.say_hi
+    mg.say_bye
+  
+    # Change to nil
+    mg.names = nil
+    mg.say_hi
+    mg.say_bye
+  end
+  
